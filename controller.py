4930867e0dd03198fc1fee78828b7842668e84fa
MohammadAminParsa <39618063+MohammadAminParsa@users.noreply.github.com>
2025-06-29 07:53:32 +0330

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,12 @@
+status = "ON"
+
+def get_status():
+    return status
+
+def set_status(s: str):
+    global status
+    s = s.upper()
+    if s in ["ON", "OFF"]:
+        status = s
+        return True
+    return False
