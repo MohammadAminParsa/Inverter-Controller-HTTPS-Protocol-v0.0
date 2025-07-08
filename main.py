74ed56d7abc05577fc2d11fe6c73a07a9de0129d
MohammadAminParsa <39618063+MohammadAminParsa@users.noreply.github.com>
2025-07-08 13:28:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # main.py
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+# ساختار داده‌ای که از ESP32 دریافت می‌شود
 class InverterData(BaseModel):
     timestamp: str
     voltage_1: float
@@ -13,7 +15,14 @@ class InverterData(BaseModel):
     status: int
     error_text: str
 
+# حافظه موقت برای ذخیره داده‌ها
+data_log: List[InverterData] = []
+
 @app.post("/data")
 async def receive_data(data: InverterData):
-    # اینجا می‌تونی ذخیره در دیتابیس، فایل یا پردازش دیگه انجام بدی
+    data_log.append(data)
     return {"success": True}
+
+@app.get("/data")
+async def get_all_data():
+    return data_log
